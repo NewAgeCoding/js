31380cd846f8f11ae60359cfd9afb6a1b2d6cc0c
BasicHost <33496604+NewAgeCoding@users.noreply.github.com>
2022-06-15 09:33:32 +0000

    Create indie1.js

---
+++ indie1.js
@@ -0,0 +1,20 @@
+class Indie {
+  constructor(namez, genre, des, votes, video) {
+    this.namez = namez
+    this.genre = genre
+    this.des = des
+    this.votes = votes
+    this.video = video
+
+  }
+}
+
+plantDown = new Car("Plant Down", 
+"Arcade", 
+`In Plant Down, you're faced with 3 bosses of increasing difficulty. Will you manage to take them all down?
+<BR><BR>
+â€¢ The game takes between 20 minutes and one hour to complete
+<BR>
+â€¢ Plant Down uses an elegant two-button input system designed for mobile
+<BR>
+â€¢ The game contains little to no text`, 0, "");
